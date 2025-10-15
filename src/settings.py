0eb317cb4b2c7218fbbc3664d75c1d64e8debc47
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-10-15 11:40:15 +0300

    fix settings

--- src/settings.py
+++ src/settings.py
@@ -5,19 +5,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = Path(__file__).parent.parent
 
 
-class PostgresSettings(BaseSettings):
+class CoreBaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=f'{BASE_DIR}/environment/.env', extra='ignore'
+    )
+
+
+class PostgresSettings(CoreBaseSettings):
     POSTGRES_USER: str = ''
     POSTGRES_PASSWORD: str = ''
     POSTGRES_DB: str = ''
     POSTGRES_HOST: str = ''
     POSTGRES_PORT: int = 5432
 
-    model_config = SettingsConfigDict(
-        env_file=f'{BASE_DIR}/environment/.env', extra='ignore'
-    )
-
     @property
-    def POSTGRES_URL(self) -> str:
+    def POSTGRES_ASYNC_URL(self) -> str:
         return (
             f'postgresql+asyncpg://{self.POSTGRES_USER}'
             f':{self.POSTGRES_PASSWORD}'
@@ -26,8 +28,18 @@ class PostgresSettings(BaseSettings):
             f'/{self.POSTGRES_DB}'
         )
 
+    @property
+    def POSTGRES_SYNC_URL(self) -> str:
+        return (
+            f'postgresql+psycopg2://{self.POSTGRES_USER}'
+            f':{self.POSTGRES_PASSWORD}'
+            f'@{self.POSTGRES_HOST}'
+            f':{self.POSTGRES_PORT}'
+            f'/{self.POSTGRES_DB}'
+        )
 
-class Settings(BaseSettings):
+
+class Settings(CoreBaseSettings):
     postgres: PostgresSettings = PostgresSettings()
 
 
