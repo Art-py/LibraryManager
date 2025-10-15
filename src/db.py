25ba42d78d6ca26f9beebb7c4c8b5a55f9542689
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-10-15 15:14:28 +0300

    add AsyncGenerator

---
+++ src/db.py
@@ -0,0 +1,13 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from settings import settings
+
+engine = create_async_engine(settings.postgres.POSTGRES_ASYNC_URL)
+AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
