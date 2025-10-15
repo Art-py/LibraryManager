64c6c5e7dcff97faa9930d17c90e5d6fdb1941be
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-10-15 17:36:40 +0300

    fix async_sessionmaker

--- src/db.py
+++ src/db.py
@@ -4,8 +4,10 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 
 from settings import settings
 
-engine = create_async_engine(settings.postgres.POSTGRES_ASYNC_URL)
-AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(settings.postgres.POSTGRES_ASYNC_URL, echo=settings.DEBUG)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
+)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
