003b0493959ec259db8dd2b31e5e9351a4a04934
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-10-17 14:29:14 +0300

    fix ruff error

--- src/db.py
+++ src/db.py
@@ -5,9 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from settings import settings
 
 engine = create_async_engine(settings.postgres.POSTGRES_ASYNC_URL, echo=settings.DEBUG)
-AsyncSessionLocal = async_sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
-)
+AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
