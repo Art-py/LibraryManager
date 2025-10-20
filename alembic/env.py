160fe1ed3fba2ac8d219b575581045e947f7061f
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-10-20 11:58:22 +0300

    add user model in alembic

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from settings import settings
 from src.repositories.core.base_model import BaseModel
+from src.repositories.users.model import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
