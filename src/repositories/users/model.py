d10b9ad72edf2c70f45a5bc0d5567bc59df3a604
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-10-23 14:11:56 +0300

    fix user model

--- src/repositories/users/model.py
+++ src/repositories/users/model.py
@@ -4,15 +4,14 @@ from sqlalchemy.orm import Mapped, mapped_column
 from repositories.users.enum import UserRole
 from src.repositories.core.base_model import BaseModel, TimestampsMixin
 
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 
-class User(BaseModel, TimestampsMixin):
+
+class User(BaseModel, SQLAlchemyBaseUserTableUUID, TimestampsMixin):
     """Модель пользователей"""
-
-    __tablename__ = 'users'
 
     first_name: Mapped[str] = mapped_column(String(255), nullable=False, index=True)
     last_name: Mapped[str] = mapped_column(String(255), nullable=False, index=True)
     second_name: Mapped[str | None] = mapped_column(String(255), nullable=True)
 
-    email: Mapped[str] = mapped_column(String(128), nullable=False, unique=True)
     role: Mapped[UserRole] = mapped_column(Enum(UserRole), nullable=False, default=UserRole.READER)
