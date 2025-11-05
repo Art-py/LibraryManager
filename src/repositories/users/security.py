edbee2d4cafbf72e67428fdef5eb53fbcbb267e6
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-11-05 12:01:25 +0300

    add sync method

--- src/repositories/users/security.py
+++ src/repositories/users/security.py
@@ -4,10 +4,12 @@ from pydantic.types import SecretStr
 from src.settings import settings
 
 
-
 class SecurityService:
     secret = settings.SECRET_KEY_PASSWORD.get_secret_value()
     pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
     async def get_hashed_password(self, password: SecretStr) -> str:
+        return self.pwd_context.hash(password.get_secret_value())
+
+    def get_hashed_password_sync(self, password: SecretStr) -> str:
         return self.pwd_context.hash(password.get_secret_value())
