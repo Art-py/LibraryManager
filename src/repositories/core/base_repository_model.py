3ad07b37a62f081010fc0d1ad92608d6df91b70d
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-11-20 12:56:16 +0300

    base repository extension

--- src/repositories/core/base_repository_model.py
+++ src/repositories/core/base_repository_model.py
@@ -1,6 +1,9 @@
 from abc import ABC
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.db import get_async_session
 
 
 class BaseRepository(ABC):
@@ -8,3 +11,7 @@ class BaseRepository(ABC):
 
     def __init__(self, session: AsyncSession):
         self._session: AsyncSession = session
+
+    @classmethod
+    async def get_dependency(cls, session: AsyncSession = Depends(get_async_session)):
+        return cls(session=session)
