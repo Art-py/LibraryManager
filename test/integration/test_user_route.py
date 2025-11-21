f2e4198bd4f3d02382230f09da2026fd5cd826ae
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-11-21 14:46:48 +0300

    add new test

--- test/integration/test_user_route.py
+++ test/integration/test_user_route.py
@@ -1,7 +1,9 @@
 import pytest
+import uuid6
 from fastapi import status
 from httpx import AsyncClient
 
+from src.adapters.api.v1.users.schema import UserResponse
 from src.repositories.users.enum import UserRole
 from src.repositories.users.model import User
 
@@ -18,4 +20,34 @@ class TestUserRoute:
 
         assert response.status_code == status.HTTP_200_OK
         data = response.json()
-        print(data)
+
+        response_user = UserResponse(**data)
+
+        assert response_user.uid == user.uid
+        assert response_user.email == user.email
+        assert response_user.first_name == user.first_name
+        assert response_user.last_name == user.last_name
+        assert response_user.second_name == user.second_name
+        assert response_user.role == user.role
+        assert response_user.is_active == user.is_active
+        assert response_user.is_superuser == user.is_superuser
+        assert response_user.is_verified == user.is_verified
+
+    @pytest.mark.asyncio
+    async def test_get_user_by_uid_failure(self, client: AsyncClient):
+        response = await client.get(
+            url=f'{USER_URL}/{uuid6.uuid7()}',
+        )
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+        data = response.json()
+        assert data['detail'] == 'User not found'
+
+    @pytest.mark.asyncio
+    async def test_get_user_by_wrong_uid(self, client: AsyncClient):
+        response = await client.get(
+            url=f'{USER_URL}/{666}',
+        )
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
