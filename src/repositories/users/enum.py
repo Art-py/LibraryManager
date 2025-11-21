cb1b74b2b25f6d508925851ec0eba2d6ff6b89e8
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-11-21 13:15:44 +0300

    fix user role enum

--- src/repositories/users/enum.py
+++ src/repositories/users/enum.py
@@ -1,7 +1,6 @@
-import enum
+from enum import Enum
 
-
-class UserRole(enum.Enum):
+class UserRole(str, Enum):
     READER = 'Reader'
     LIBRARIAN = 'Librarian'
     ADMINISTRATOR = 'Administrator'
