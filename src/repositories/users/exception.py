69cc420fb8967f150d539f2cb4a1a20df8570393
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-11-24 13:58:20 +0300

    add user exception

---
+++ src/repositories/users/exception.py
@@ -0,0 +1,13 @@
+from fastapi import status
+
+from src.repositories.core.base_exception import BaseException
+
+
+class PasswordNotConfirm(BaseException):
+    def __init__(self, message: str):
+        super().__init__(code=status.HTTP_401_UNAUTHORIZED, message=message)
+
+
+class UserIsRegistered(BaseException):
+    def __init__(self, message: str):
+        super().__init__(code=status.HTTP_409_CONFLICT, message=message)
