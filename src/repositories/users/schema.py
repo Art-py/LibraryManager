3dfe2d6d3b1bee4fef7f26faf42b76c84589b417
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-11-24 13:59:52 +0300

    extension UserCreate

---
+++ src/repositories/users/schema.py
@@ -0,0 +1,42 @@
+from typing import Self
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, model_validator
+
+from src.repositories.users.enum import UserRole
+from src.repositories.users.exception import PasswordNotConfirm
+from src.repositories.users.security import SecurityService
+
+security_service = SecurityService()
+
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    second_name: str | None
+
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, max_length=50, description='Пароль, от 8 до 50 знаков')
+    password_confirm: str = Field(min_length=8, max_length=50, description='Пароль, от 8 до 50 знаков')
+
+    @model_validator(mode='after')
+    def check_password(self) -> Self:
+        if self.password != self.password_confirm:
+            raise PasswordNotConfirm(message='Password not confirm')
+        self.password_confirm = security_service.get_hashed_password_sync(self.password)
+        return self
+
+
+class UserResponse(UserBase):
+    uid: UUID
+
+    role: UserRole
+
+    is_active: bool
+    is_superuser: bool
+    is_verified: bool
+
+    model_config = ConfigDict(from_attributes=True)
