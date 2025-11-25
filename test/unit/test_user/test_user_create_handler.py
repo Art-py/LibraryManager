f471d0d8a625f1f5c8820192db7108eb3c41cf61
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-11-25 11:30:45 +0300

    add new test for user handler

--- test/unit/test_user/test_user_create_handler.py
+++ test/unit/test_user/test_user_create_handler.py
@@ -1,8 +1,10 @@
 import pytest
 import pytest_asyncio
+from pydantic import ValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.applications.users.create_user_handler import CreateUserHandler
+from src.domains.users.exception import PasswordNotConfirm, UserIsRegistered
 from src.domains.users.schema import UserCreate
 from src.domains.users.uow import UserUOW
 from test.core.factories.user import UserFactory
@@ -10,6 +12,7 @@ from test.core.factories.user import UserFactory
 
 @pytest_asyncio.fixture()
 async def user_handler(sql_test_session: AsyncSession) -> CreateUserHandler:
+    """Получение хэндлера для создания пользователя"""
     return CreateUserHandler(unit=UserUOW(session=sql_test_session))
 
 
@@ -37,3 +40,70 @@ class TestUsersHandler:
         assert not created_user.is_active
         assert not created_user.is_superuser
         assert created_user.is_verified
+
+    @pytest.mark.asyncio
+    async def test_user_create_little_password(self, user_handler: CreateUserHandler):
+        user_data = UserFactory.build()
+
+        create_data = {
+            'first_name': user_data.first_name,
+            'last_name': user_data.last_name,
+            'second_name': None,
+            'email': user_data.email,
+            'password': '123',
+            'password_confirm': '123',
+        }
+
+        with pytest.raises(ValidationError) as _:
+            await user_handler.handle(UserCreate(**create_data))
+
+    @pytest.mark.asyncio
+    async def test_user_create_not_confirm_password(self, user_handler: CreateUserHandler):
+        user_data = UserFactory.build()
+
+        create_data = {
+            'first_name': user_data.first_name,
+            'last_name': user_data.last_name,
+            'second_name': None,
+            'email': user_data.email,
+            'password': '1234567890',
+            'password_confirm': '123456789',
+        }
+
+        with pytest.raises(PasswordNotConfirm) as _:
+            await user_handler.handle(UserCreate(**create_data))
+
+    @pytest.mark.asyncio
+    async def test_user_create_failure(self, user_handler: CreateUserHandler):
+        # Сначала создаем пользователя как обычно
+        user_data = UserFactory.build()
+
+        email = 'example123@example.com'
+
+        create_data = {
+            'first_name': user_data.first_name,
+            'last_name': user_data.last_name,
+            'second_name': None,
+            'email': email,
+            'password': '123123123',
+            'password_confirm': '123123123',
+        }
+
+        created_user = await user_handler.handle(UserCreate(**create_data))
+
+        assert created_user.uid is not None
+
+        # Теперь пытаемся добавить пользователя с такой же почтой
+        user_data = UserFactory.build()
+
+        create_data = {
+            'first_name': user_data.first_name,
+            'last_name': user_data.last_name,
+            'second_name': None,
+            'email': email,
+            'password': '123123123',
+            'password_confirm': '123123123',
+        }
+
+        with pytest.raises(UserIsRegistered) as _:
+            await user_handler.handle(UserCreate(**create_data))
