e9d04929e939116b0481a06bfd739d0da3ce20c1
Zhukov Artem <zo.sahaal.13@gmail.com>
2025-12-03 12:31:00 +0300

    fix settings in BaseRadisRepository

--- src/domains/core/base_redis_repository.py
+++ src/domains/core/base_redis_repository.py
@@ -2,13 +2,13 @@ from fastapi import Depends
 from redis.asyncio import Redis
 
 from src.db import get_redis_client
-from src.settings import RedisSettings, get_redis_settings
+from src.settings import PasswordSettings, get_password_settings
 
 
 class BaseRedisRepository:
     def __init__(self, redis_client: Redis):
         self._redis_client: Redis = redis_client
-        self._settings: RedisSettings = get_redis_settings()
+        self._settings: PasswordSettings = get_password_settings()
 
     @classmethod
     async def get_dependency(cls, redis_client: Redis = Depends(get_redis_client)):
